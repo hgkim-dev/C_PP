dbb3080f34edd7c272bd489fb890edd94e848a10
hgkim-dev <heyoul51@gmail.com>
2021-06-10 16:37:17 +0900

    2021-06-10 c++ study

--- jun_10/main.cpp
+++ jun_10/main.cpp
@@ -1,8 +1,19 @@
 #include <cstdio>
 
 int main (int argc, char* argv[]) {
+/*
+int a;
 
-printf("Hello World!");
+scanf("%d", &a);
+
+printf("%c", a);*/
+
+
+int a,b;
+
+scanf("%ld %ld",&a, &b);
+
+printf("%ld", a+b);
 
 return 0;
 } 
