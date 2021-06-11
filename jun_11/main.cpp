e8f9d03d344591d00c71f92e7c8a4ad9684057f0
hgkim-dev <heyoul51@gmail.com>
2021-06-11 15:40:07 +0900

    2021-06-11 C++

---
+++ jun_11/main.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+int main (int argc, char* argv[]) {
+/*
+	long long int a,b;
+	
+	scanf("%lld %lld", &a,&b);
+	
+	printf("%lld", -(a+b));
+*/
+
+	char a;
+	
+	scanf("%c", &a);
+	
+	printf("%c", a+1);
+	return 0;
+}
